e6b9324f5b59704cc6a0f4e64b13b51c782c5cab
mrtnwh <lagonegromartin@gmail.com>
2023-12-08 08:26:51 -0300

    Inicio proyecto

---
+++ src/main/java/ar/edu/utn/frbb/tup/Laboratorio3Final/Laboratorio3FinalApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frbb.tup.Laboratorio3Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio3FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio3FinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/frbb/tup/Laboratorio3Final/Laboratorio3FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frbb.tup.Laboratorio3Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio3FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
