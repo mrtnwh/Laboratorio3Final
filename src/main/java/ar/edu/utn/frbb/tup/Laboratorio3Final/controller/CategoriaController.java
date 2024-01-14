47c66051fe7faae796a5008bd9d26d361007c1ed
mrtnwh <lagonegromartin@gmail.com>
2024-01-14 12:49:55 -0300

    agrega capa controller

--- src/main/java/ar/edu/utn/frbb/tup/Laboratorio3Final/controller/CategoriaController.java
+++ src/main/java/ar/edu/utn/frbb/tup/Laboratorio3Final/controller/CategoriaController.java
@@ -1,4 +1,90 @@
 package ar.edu.utn.frbb.tup.Laboratorio3Final.controller;
 
+import ar.edu.utn.frbb.tup.Laboratorio3Final.business.CategoriaBusiness;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.business.ProductoBusiness;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.dto.AltaCategoriaDto;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.dto.AltaProductoDto;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.model.Categoria;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.model.Producto;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.singletons.CategoriaBusinessImplementationSingleton;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.singletons.ProductoBusinessImplementationSingleton;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class CategoriaController {
+    @Autowired
+    CategoriaBusiness categoriaBusiness = CategoriaBusinessImplementationSingleton.getInstance();
+
+    ProductoBusiness productoBusiness = ProductoBusinessImplementationSingleton.getInstance();
+
+
+    @ManagedOperation(description = "Crea una nueva categoria")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/categoria", method= RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public Categoria crearCategoria(@RequestBody AltaCategoriaDto categoriaDto) {
+        return categoriaBusiness.altaCategoria(categoriaDto);
+    }
+
+    @ManagedOperation(description = "Elimina una categoria")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/categoria/{id}", method= RequestMethod.DELETE)
+    public String eliminarCategoria(@PathVariable("id") int id) {
+        AltaCategoriaDto categoriaDto = new AltaCategoriaDto(id);
+        return categoriaBusiness.bajaCategoria(categoriaDto) ? "la cateogoria fue eliminada, con el id "+ id : "no se encontro ninguna categoria con id "+ id + " o no se puede borrar, porque tiene productos";
+    }
+
+    @ManagedOperation(description = "Editar una categoria")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/categoria/{id}", method= RequestMethod.PUT, consumes = "application/json", produces = "application/json")
+    public String editarCategoria(@PathVariable("id") int id, @RequestBody AltaCategoriaDto categoriaDto) {
+        //TODO:cuando hago el edit, se borran los productos asociados a la categoria
+        if (categoriaBusiness.consultarCategoriaPorId(id) == null) {
+            return "no existe ninguna categoria con el id "+ id;
+        }
+        return categoriaBusiness.modificarCategoria(categoriaDto).toString();
+    }
+
+    @ManagedOperation(description = "Obtener una categoria por id")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/categoria/{id}", method= RequestMethod.GET, produces = "application/json")
+    public String obtenerCategoria(@PathVariable("id") int id) {
+        if (categoriaBusiness.consultarCategoriaPorId(id) == null) {
+            return "no existe ninguna categoria con el id "+ id;
+        }
+        else
+            return categoriaBusiness.consultarCategoriaPorId(id).toString();
+
+    }
+
+
+    @ManagedOperation(description = "Obtener todos los productos ordenados por parametro opcional")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/categoria", method= RequestMethod.GET)
+    public String obtenerProductosOrdenadosPorPrecio(@RequestBody AltaCategoriaDto categoriaDto,
+                                                     @RequestParam(value = "order_price", required = false) String order_price,
+                                                     @RequestParam(value = "marca", required = false) String marca,
+                                                     @RequestParam(value = "maximo", required = false) Integer maximo,
+                                                     @RequestParam(value = "minimo", required = false) Integer minimo) {
+        if (order_price != null) {
+        if (order_price.equals("asc")) {
+            return categoriaBusiness.getProductosOrdenadosPorPrecioAsc(categoriaDto).toString();
+        }
+        else if (order_price.equals("desc")) {
+        return categoriaBusiness.getProductosOrdenadosPorPrecioDesc(categoriaDto).toString();
+        }
+    }
+        if (marca !=null) {
+            return categoriaBusiness.getProductosPorMarca(categoriaDto, marca).toString();
+        }
+
+        if (minimo != 0 && maximo != 0) {
+            return categoriaBusiness.getProductosFiltradosPorPrecios(categoriaDto,minimo,maximo).toString();
+        }
+        else
+            return "malos parametros";
+    }
 }
+
--- src/main/java/ar/edu/utn/frbb/tup/Laboratorio3Final/controller/ProductoController.java
+++ src/main/java/ar/edu/utn/frbb/tup/Laboratorio3Final/controller/ProductoController.java
@@ -1,4 +1,86 @@
 package ar.edu.utn.frbb.tup.Laboratorio3Final.controller;
 
+import ar.edu.utn.frbb.tup.Laboratorio3Final.business.CategoriaBusiness;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.business.Implementation.CategoriaBusinessImplementation;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.business.ProductoBusiness;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.dto.AltaCategoriaDto;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.dto.AltaProductoDto;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.model.Producto;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.singletons.CategoriaBusinessImplementationSingleton;
+import ar.edu.utn.frbb.tup.Laboratorio3Final.singletons.ProductoBusinessImplementationSingleton;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
 public class ProductoController {
+    @Autowired
+    ProductoBusiness productoBusiness = ProductoBusinessImplementationSingleton.getInstance();
+    @Autowired
+    CategoriaBusiness categoriabusiness = CategoriaBusinessImplementationSingleton.getInstance();
+
+    @ManagedOperation(description = "Crear Producto")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/producto", method = RequestMethod.POST, consumes = "application/json")
+    public String crearProducto(@RequestBody AltaProductoDto productoDto, @RequestParam(value = "idCategoria") int idCategoria) {
+        AltaCategoriaDto categoriaDto = new AltaCategoriaDto("test categoria", "test descripcion");
+        categoriaDto.setId(idCategoria);
+        if (categoriabusiness.consultarCategoriaPorId(categoriaDto.getId()) == null) {
+            return "No existe una categoria con el id " + idCategoria;
+        }
+        return "Creo el producto: "+productoBusiness.altaProducto(productoDto, categoriaDto).toString();
+    }
+
+    @ManagedOperation(description = "Eliminar Producto")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/producto/{id}", method = RequestMethod.DELETE)
+    public String eliminarProducto(@PathVariable int id) {
+        AltaProductoDto productoDto = new AltaProductoDto(id);
+        return productoBusiness.bajaProducto(productoDto) ? "El producto fue eliminado, con el id " + id : "No fue encontrada ningun producto con el id " + id;
+    }
+
+    @ManagedOperation(description = "Editar Producto")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/producto/{id}", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
+    public String editarProducto(@PathVariable int id, @RequestBody AltaProductoDto productoDto) {
+        if (productoBusiness.getProductoPorId(id) == null) {
+            return "No fue encontrada ningun producto con el id " + id;
+        }
+        if (categoriabusiness.consultarCategoriaPorId(productoDto.getCategoriaId())!= null) {
+            return "no fue encontrada ninguna cateogoria con el id " + productoDto.getCategoriaId();
+        }
+        return "se pudo editar correctamente el producto: "+productoBusiness.modificarProducto(productoDto).toString();
+    }
+
+    @ManagedOperation(description = "Obtener un Producto por id")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/producto/{id}", method = RequestMethod.GET, produces = "application/json")
+    public String editarProducto(@PathVariable int id) {
+        if (productoBusiness.getProductoPorId(id) == null) {
+            return "No fue encontrada ningun producto con el id " + id;
+        }
+        return productoBusiness.getProductoPorId(id).toString();
+    }
+
+    /*
+     Consultar un producto por alguno de sus atributos: tipo, marca, categoria
+        GET: /producto?tipo_producto=dvd&marca=samsung&cateogoria=ATV
+        GET: /producto?tipo_producto=tv&marca=phillips&categoria=ATV
+     */
+    @ManagedOperation(description = "Consultar un Producto por alguno de sus atributos: tipo, marca, categoria")
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/producto", method = RequestMethod.GET)
+    public String consultarProducto(@RequestParam(value = "tipo", required = false) String tipo, @RequestParam(value = "marca", required = false) String marca, @RequestParam(value = "categoria", required = false) String categoria){
+        if (Objects.equals(tipo, "") && Objects.equals(marca, "") && Objects.equals(categoria, "")) {
+            return "No se recibieron parametros para la consulta";
+        }
+        List<Producto> productos = productoBusiness.getProductosPorAtributos(tipo, marca, categoria);
+        return productos.isEmpty() ? "No se encontraron productos con los parametros de busqueda" : productos.toString();
+    }
+
+
 }
